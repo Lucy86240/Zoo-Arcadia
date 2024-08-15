176d23bcfb6da1af05791b535e6214128a8fcae0
Lucy86240 <raye.lucy@live.fr>
2024-08-15 10:18:17 +0200

    add frontend dashboard

---
+++ View/pages/dashboard.php
@@ -0,0 +1,42 @@
+<section id="dashboard">
+    <div id="popular">
+
+    </div>
+    <div id="tiles">
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/users.svg">
+            Comptes utilisateurs
+        </div>
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/time.svg">
+            Horaires du zoo
+        </div>
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/empreinte.png">
+            empreinte
+            Animaux
+        </div>
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/Star-gold.png">
+            Avis
+        </div>
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/feed.svg">
+            Repas des animaux
+        </div>
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/veterinaire.png">
+            Comptes-rendus vétérinaires
+        </div>
+        <div class="tile">
+            <img src ="View/assets/img/general/page/dashboard/housing.svg">
+            Commentaires des habitats
+        </div>
+    </div>
+
+
+
+
+
+
+</section>
