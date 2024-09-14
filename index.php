93f64ce0f69399b12e795e6c4697bcc49dbf4938
Lucy86240 <raye.lucy@live.fr>
2024-09-14 19:48:48 +0200

    test index

--- index.php
+++ index.php
@@ -1,22 +1,38 @@
 <?php
     session_start();
-    require_once 'CONSTANTES.php';
-    require_once 'config.php';
-    require_once 'fonctions_generals_and_initializations.php';
-    require_once 'router/router.php';
-    $optionPage=optionPage($request);?> 
-    <script src="<?php if(isset($optionPage) && $optionPage){echo("../");}?>CONSTANTES_inputs.js"></script> 
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <?php
-    require_once 'View/elements/header.php';
+    try{
+        require_once 'CONSTANTES.php';
+        require_once 'config.php';
+        require_once 'fonctions_generals_and_initializations.php';
+    }catch(error $e){
+        echo('1');
+    }
+    try{
+        require_once 'router/router.php';
+    }catch(error $e){
+        echo("2");
+    }
+
+    try{
+
+    }catch(error $e){
+        $optionPage=optionPage($request);?> 
+        <script src="<?php if(isset($optionPage) && $optionPage){echo("../");}?>CONSTANTES_inputs.js"></script> 
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+        <?php
+        require_once 'View/elements/header.php';
+    }
     
     try{
         loadContentPage($request);
     }
     catch(error $e){
-        echo("router merde");
+        echo("3");
     }
 
-    
-    require_once 'View/elements/footer.php';   
+    try{
+        require_once 'View/elements/footer.php';  
+    }catch(error $e){
+        echo("4");
+    }
 
