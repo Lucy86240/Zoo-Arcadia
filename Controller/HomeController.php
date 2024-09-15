45095dd1a3314ba0a6f338a01d8296aa6cd88e26
Lucy86240 <raye.lucy@live.fr>
2024-09-15 10:38:25 +0200

    test home

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -2,11 +2,11 @@
 //si on est sur un url différent du chemin du fichier on execute le programme
 if($_SERVER['REQUEST_URI']!='/Controller/homeController.php'){
         //on récupère les infos des habitats pour la section habitat
-        require_once "Controller/manageHousing.php";
+        //require_once "Controller/manageHousing.php";
         //on récupère les infos des services pour la section services
-        require_once "Controller/manageService.php";
+        //require_once "Controller/manageService.php";
         //on récupère les infos des avis pour la section avis
-        require_once "Controller/manageReview.php";
+        //require_once "Controller/manageReview.php";
 
 }
 else{
